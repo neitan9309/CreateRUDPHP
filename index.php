e896b254dfddfeb6f1182e683a52d7342553be65
neitan9309 <134731894+neitan9309@users.noreply.github.com>
2024-08-21 20:57:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <?php
+    //Comando que inclui o banco de dados configurado no arquivo index.
     include("database.php");
 
+    //Variáveis, uma para armazenar o usuário inserido, outro para a senha e a $hash que conterá a senha encriptada.
     $usuario = "Neitan";
     $senha = "93098840";
-    $hash = password_hash($senha, PASSWORD_DEFAULT);
+    $hash = password_hash($senha, PASSWORD_DEFAULT);/*
+                                                    aqui temos a função password_hash(___, PASSWORD_DEFAULT)
+                                                    responsável por encriptar nossa senha (e também de
+                                                    possibilitar a desencriptação).
+                                                    */
 
+    //Comando que insere as informações fornecidas nas suas respectivas colunas da tabela usuarios.
     $sql = "INSERT INTO usuarios (usuario, senha)
             VALUES ('$usuario', '$hash')";
-
+    
+    /*Comando que da o gatilho para o código SQL
+    rodar no banco de dados. O try/ catch, serve
+    para evitar que textos muito grandes de erro
+    apareçam, caso a conexão com o banco não ocorra. */
     try{
-        mysqli_query($conn, $sql);
-        echo"O usuário foi registrado";
-    }
-    catch(mysqli_sql_exception){
+        mysqli_query($conn, $sql);                                                  
+        echo"O usuário foi registrado";                                         
+    }                                                                               
+    catch(mysqli_sql_exception){                                                
         echo"O usuário não pôde registrado";
     }
 
+    //Fechar conexão.
     mysqli_close($conn);
 ?>
 <!DOCTYPE html>
