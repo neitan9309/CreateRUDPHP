cc16547891c8e8112b8dd603cf814928e22c9e4e
neitan9309 <134731894+neitan9309@users.noreply.github.com>
2024-08-21 21:19:51 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,11 +1,14 @@
 <?php
-
+    //Variáveis necessáris para configurar nosso banco de dados.
     $db_server = "localhost";
     $db_user = "root";
     $db_pass = "";
-    $db_name = "praticadb";
+    $db_name = "usuarios";
     $conn = "";
-    
+
+    /*Função mysqli_connect, que serve para estabelecer
+    a conexão com o bd. O try/ catch serve para evitar
+    grandes textos de erro, caso a conexão não ocorra.*/
     try{
         $conn = mysqli_connect($db_server,
                                 $db_user,
@@ -15,4 +18,5 @@
     catch(mysqli_sql_exception){
         echo"Erro na conexão!";
     }
+    
 ?>
